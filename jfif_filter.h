cc9ffb96df54f32bc483812f3f7e55180df36dac
kricnam <kricnam@yahoo.com>
2016-01-23 23:10:52 +0800

    Update jfif_filter.h

--- jfif_filter.h
+++ jfif_filter.h
@@ -10,6 +10,6 @@
 // filter away no standard markers of JFIF in the input string,
 // which is the content of a jfif file in binary mode
 // the return string is the binary data of the picture
-// without no-standard marks.
+// only with standard markers.
 std::string filter_stream(const std::string &strIn);
 #endif /* JFIF_FILTER_H_ */
